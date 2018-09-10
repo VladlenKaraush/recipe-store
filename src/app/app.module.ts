5514567b05646c6f39c059a107a0017e095f43ed
Vladlen <vladlenkaraush@gmail.com>
2018-09-10 06:06:34 +0300

    added appRoutes module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireAuthModule } from 'angularfire2/auth';
+import {HttpModule} from '@angular/http';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireAuthModule} from 'angularfire2/auth';
 import {AngularFireDatabaseModule} from "angularfire2/database";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
 
 import {ShoppingListService} from "./shopping-list/shoppingListService";
-import {RouterModule, Routes} from "@angular/router";
 import {RecipeService} from "./recipes/recipeService";
 import {environment} from "../environments/environment";
 import {DataStorageService} from "./shared/data-storage.service";
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
+import {SignupComponent} from './auth/signup/signup.component';
+import {SigninComponent} from './auth/signin/signin.component';
 import {AuthService} from "./auth/auth.service";
 import {AuthGuard} from "./auth/auth-guard.service";
-import {DefaultRecipeComponent} from "./recipes/default-recipe/default-recipe.component";
-import {RecipeEditComponent} from "./recipes/recipe-edit/recipe-edit.component";
 import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
 import {ShoppingEditComponent} from "./shopping-list/shopping-edit/shopping-edit.component";
-import {DropdownDirective} from "./shared/dropdown.directive";
+import {AppRoutesModule} from "./app-routes.module";
+import {RecipesModule} from "./recipes/recipes.module";
 
-
-
-const appRoutes: Routes = [
-  {path: '', redirectTo: 'recipes', pathMatch: 'full'},
-  {path: 'recipes', component: RecipesComponent, children: [
-      {path: '', component: DefaultRecipeComponent},
-      {path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard]},
-      {path: ':id', component: RecipeDetailComponent},
-      {path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard]},
-    ]},
-  {path: 'shopping-list', component: ShoppingListComponent},
-  {path: 'signup', component: SignupComponent},
-  {path: 'signin', component: SigninComponent}
-];
 
 @NgModule({
   declarations: [
@@ -56,8 +37,9 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    ReactiveFormsModule, 
-    RouterModule.forRoot(appRoutes),
+    ReactiveFormsModule,
+    AppRoutesModule,
+    RecipesModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features
@@ -65,4 +47,5 @@ const appRoutes: Routes = [
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
